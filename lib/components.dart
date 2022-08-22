851a9ae0208eccad2e95efa3d08bbf1178ad9fd0
Alex Bartolo <alexander.j.bartolo.18@um.edu.mt>
2022-08-22 07:33:36 +0200

    Added text input

--- lib/components.dart
+++ lib/components.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class TextOutput extends StatelessWidget {
   final String textOutputBody;
   final String textOutputStyle;
+
   const TextOutput(this.textOutputBody, this.textOutputStyle, {Key? key}) : super(key: key);
 
   @override
@@ -11,21 +14,25 @@ class TextOutput extends StatelessWidget {
   }
 }
 
-class Input extends StatelessWidget {
+class TextInput extends StatelessWidget {
   final String inputName;
-  final Icon inputIcon;
-  final TextEditingController inputController;
-  const Input(this.inputName, this.inputIcon, this.inputController, {Key? key}) : super(key: key);
+  // final Icon inputIcon;
+  final String inputType;
+
+//this.inputIcon,
+  const TextInput(this.inputName, this.inputType, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      inputFormatters: [textInputInformation[inputType]!.textEditingFormatter],
+      controller: textInputInformation[inputType]!.textEditingController,
+      keyboardType: textInputInformation[inputType]!.textInputType,
+      decoration: InputDecoration(
+        labelText: inputName
+      ),
+    );
   }
-}
-
-class TextInput extends Input {
-  const TextInput(inputName, inputIcon, inputController, {Key? key}) : super(inputName, inputIcon, inputController, key:key);
-
 }
 
 const Map<String, TextStyle> textStyles = {
@@ -38,3 +45,20 @@ const Map<String, TextStyle> textStyles = {
   "Button": TextStyle(fontSize: 32, fontWeight: FontWeight.bold)
 };
 
+class TextInputInformation {
+  final TextInputType textInputType;
+  final TextEditingController textEditingController = TextEditingController();
+  late final TextInputFormatter textEditingFormatter;
+
+  TextInputInformation.withFormatter(this.textInputType, this.textEditingFormatter);
+  TextInputInformation.withoutFormatter(this.textInputType) {textEditingFormatter = MaskTextInputFormatter();}
+}
+
+Map<String, TextInputInformation> textInputInformation = {
+  "date": TextInputInformation.withFormatter(TextInputType.datetime, MaskTextInputFormatter(mask: "##/##/####", filter: { "#": RegExp(r'[0-9]')})),
+  "time": TextInputInformation.withFormatter(TextInputType.datetime, MaskTextInputFormatter(mask: "##:##", filter: { "#": RegExp(r'[0-9]')})),
+  "email": TextInputInformation.withoutFormatter(TextInputType.emailAddress),
+  "phone": TextInputInformation.withoutFormatter(TextInputType.phone),
+  "name": TextInputInformation.withoutFormatter(TextInputType.name), //check
+  "normal": TextInputInformation.withoutFormatter(TextInputType.text)
+};
--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const TextOutput("Selected List Item", "Selected List Item"),
             const TextOutput("Unselected List Item", "Unselected List Item"),
             const TextOutput("Button", "Button"),
+            const TextInput("test", "name"),
             const Text(
               'You have pushed the button this many times:',
             ),

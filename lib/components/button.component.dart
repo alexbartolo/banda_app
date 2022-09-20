f99480fe4bfe8553d73284cb547c2d0d126a8fa7
Alex Bartolo <alexj.bartolo@gmail.com>
2022-09-20 23:26:20 +0200

    Fixed button components file

--- lib/components/button.component.dart
+++ lib/components/button.component.dart
@@ -2,55 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:banda_app/components/output.component.dart';
 
+// ignore: must_be_immutable
 class DateOrTimeButton extends StatelessWidget {
   final TextEditingController textEditingController;
-  Widget? buttonIcon;
-  Future Function(BuildContext)? value;
+  late Widget buttonIcon;
+  late Future Function(BuildContext) getValue;
 
-  DateOrTimeButton.date(
-      {Key? key, required this.textEditingController})
+  DateOrTimeButton.date({Key? key, required this.textEditingController})
       : super(key: key) {
     buttonIcon = const Icon(Icons.calendar_month);
-    value = (context) async {
+    getValue = (context) async {
       return DateFormat('dd/MM/yyy').format(await showDatePicker(
-              context: context,
-              initialDate: DateTime.now(),
-              firstDate: DateTime.now(),
-              lastDate: DateTime(DateTime.now().year + 1, 12, 31)) as DateTime)
-          as String Function();
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime.now(),
+          lastDate: DateTime(DateTime.now().year + 1, 12, 31)) as DateTime);
     };
   }
 
-  DateOrTimeButton.time(
-      {Key? key, required this.textEditingController})
+  DateOrTimeButton.time({Key? key, required this.textEditingController})
       : super(key: key) {
     buttonIcon = const Icon(Icons.access_time_filled);
-    value = (context) async {
+    getValue = (context) async {
       return (await showTimePicker(
               context: context, initialTime: TimeOfDay.now()))
           ?.format(context) as String;
     };
   }
-
-  // void test = () async {
-  //   switch (type) {
-  //     case "date":
-  //       textEditingController.text = DateFormat('dd/MM/yyy').format(
-  //           await showDatePicker(
-  //                   context: context,
-  //                   initialDate: DateTime.now(),
-  //                   firstDate: DateTime.now(),
-  //                   lastDate: DateTime(DateTime.now().year + 1, 12, 31))
-  //               as DateTime);
-  //       break;
-
-  //     case "time":
-  //       textEditingController.text = (await showTimePicker(
-  //               context: context, initialTime: TimeOfDay.now()))
-  //           ?.format(context) as String;
-  //       break;
-  //   }
-  // };
 
   @override
   Widget build(BuildContext context) {
@@ -60,52 +38,15 @@ class DateOrTimeButton extends StatelessWidget {
             height: 62,
             width: 62,
             child: IconButton(
-                onPressed: () async {textEditingController.text = value!(context) as String;},
+                onPressed: () async {
+                  textEditingController.text = await getValue(context);
+                },
                 style: IconButton.styleFrom(
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20)),
                     side: const BorderSide(color: Colors.grey)),
-                icon: buttonIcon as Widget)));
+                icon: buttonIcon)));
   }
-}
-
-Widget dateOrTimeButton(
-    {required BuildContext context,
-    required String type,
-    required TextEditingController textEditingController}) {
-  return Padding(
-      padding: const EdgeInsets.only(left: 8),
-      child: SizedBox(
-          height: 62,
-          width: 62,
-          child: IconButton(
-              onPressed: () async {
-                switch (type) {
-                  case "date":
-                    textEditingController.text = DateFormat('dd/MM/yyy').format(
-                        await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime.now(),
-                                lastDate:
-                                    DateTime(DateTime.now().year + 1, 12, 31))
-                            as DateTime);
-                    break;
-
-                  case "time":
-                    textEditingController.text = (await showTimePicker(
-                            context: context, initialTime: TimeOfDay.now()))
-                        ?.format(context) as String;
-                    break;
-                }
-              },
-              style: IconButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  side: const BorderSide(color: Colors.grey)),
-              icon: type == "date"
-                  ? const Icon(Icons.calendar_month)
-                  : const Icon(Icons.access_time_filled))));
 }
 
 class PrimaryButton extends StatelessWidget {

d79ce4b766ddcbf56ad6af9204220177c6357b24
Alex Bartolo <alexj.bartolo@gmail.com>
2022-10-13 21:54:56 +0200

    Added validation for Dropdown

--- lib/utils/validations.utils.dart
+++ lib/utils/validations.utils.dart
@@ -1,7 +1,9 @@
 RegExp timeRegExp = RegExp(r"^([0-1][0-9]|[2][0-3]):([0-5][0-9])$");
-RegExp dateRegExp = RegExp(r"^(((0[1-9]|[12][0-9]|3[01])[- /.](0[13578]|1[02])|(0[1-9]|[12][0-9]|30)[- /.](0[469]|11)|(0[1-9]|1\d|2[0-8])[- /.]02)[- /.]\d{4}|29[- /.]02[- /.](\d{2}(0[48]|[2468][048]|[13579][26])|([02468][048]|[1359][26])00))$");
+RegExp dateRegExp = RegExp(
+    r"^(((0[1-9]|[12][0-9]|3[01])[- /.](0[13578]|1[02])|(0[1-9]|[12][0-9]|30)[- /.](0[469]|11)|(0[1-9]|1\d|2[0-8])[- /.]02)[- /.]\d{4}|29[- /.]02[- /.](\d{2}(0[48]|[2468][048]|[13579][26])|([02468][048]|[1359][26])00))$");
 RegExp phoneRegExp = RegExp(r"^[7|9]{2}\d{6}$");
-RegExp emailRegExp = RegExp(r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+RegExp emailRegExp = RegExp(
+    r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
 
 String? validateTime(String? time) {
   return time!.isEmpty
@@ -39,6 +41,10 @@ String? validateEmail(String? email) {
       : !emailRegExp.hasMatch(email)
           ? "Email is not valid."
           : null;
+}
+
+String? validateDropdown(String? dropdownOption) {
+  return dropdownOption == null ? "Dropdown cannot be empty" : null;
 }
 
 //https://stackoverflow.com/questions/54925779/textformfield-validator-not-working-the-method-validate-was-called-on-null
